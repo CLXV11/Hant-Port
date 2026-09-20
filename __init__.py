73b05bfc90a80550088303f42b3e838bef39f75c
ECPVX / EPCD11 <amialhnina@gmail.com>
2026-09-20 19:20:57 +0100

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,11 @@
+"""HANT PORT - a production-grade TCP port scanner for Termux/Android.
+
+No root required. TCP connect() scanning, responsible service detection,
+banner analysis, HTTP/TLS inspection, reporting, comparison and monitoring.
+"""
+
+__version__ = "1.2.1"
+AUTHOR = "CLXV11"
+GITHUB_URL = "https://github.com/CLXV11"
+APP_VERSION = __version__
+APP_NAME = "HANT PORT"
